91e81f2a53ffe6afc7c11ca5a6d41e757fcd4eb1
satishreddy955 <168215588+satishreddy955@users.noreply.github.com>
2026-01-01 09:59:44 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    const res = await axios.post("http://localhost:5000/api/auth/login", {
+    const res = await axios.post("https://chat-application-yy3j.onrender.com/api/auth/login", {
       email,
       password
     });
