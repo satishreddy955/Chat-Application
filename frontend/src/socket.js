8bd7bb87715b8bc0b85e2598bcd24483aef0772a
satishreddy955 <168215588+satishreddy955@users.noreply.github.com>
2026-01-01 10:11:45 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://chat-application-yy3j.onrender.com", {
-  transports: ["websocket", "polling"]
+const socket = io(import.meta.env.VITE_SOCKET_URL, {
+  withCredentials: true,
+  transports: ["polling", "websocket"] // ✅ allow fallback
 });
 
 export default socket;
